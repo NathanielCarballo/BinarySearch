9012848ab8d6736a96d5cbc4b9eb801b1b3178f0
Nathaniel Carballo <ncarballo@bakerripley.org>
2025-07-29 08:40:32 -0500

    Refactor: rename binarySearch to binary_search to follow PEP8

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,19 +1,16 @@
 import random
 
-def binarySearch(numberlist, l, r, searchedvalue):
-
+def binary_search(numberlist, l, r, searchedvalue):
     if r >= l:
-
         mid = l + (r - l) // 2
-
         if numberlist[mid] == searchedvalue:
             return mid
-
+        
         elif numberlist[mid] > searchedvalue:
-            return binarySearch(numberlist, l, mid-1, searchedvalue)
+            return binary_search(numberlist, l, mid-1, searchedvalue)
 
         else:
-            return binarySearch(numberlist, mid+1, r, searchedvalue)
+            return binary_search(numberlist, mid+1, r, searchedvalue)
 
     else:
         return -1
@@ -21,11 +18,10 @@ def binarySearch(numberlist, l, r, searchedvalue):
 numberlist = sorted(random.sample(range(0, 1000), 1000))
 searchedvalue = random.randint(0, 999)
 
-result = binarySearch(numberlist, 0, len(numberlist)-1,searchedvalue)
+result = binary_search(numberlist, 0, len(numberlist)-1,searchedvalue)
 
 if result != -1:
     print('Element found!')
 
 else:
     print('Not found')
-   
